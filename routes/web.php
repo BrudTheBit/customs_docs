779da9569f6386d70922408efce272f61ec6cf7c
BTG <dovbenko9910@gmail.com>
2025-08-22 12:41:52 +0300

    Test controller

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    //return view('welcome');
-    return view("hello");
-});
+Route::get('/hello', [\App\Http\Controllers\HelloPageController::class, 'index']);
